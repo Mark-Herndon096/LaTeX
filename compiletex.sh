20d5a877e98c4dab19aee0f33e90ebb2c7049653
markherndon <mah218@lehigh.edu>
2021-02-07 18:21:36 -0500

    First commit

---
+++ compiletex.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Compile LaTeX document into PDF
+DOCNAME="${1%.*}"
+pdflatex $DOCNAME.tex
+if [ $? -ne 0 ]; then
+	echo "Compileration error. Check log."
+	exit 1
+fi
+
+if [ $2 == 1 ]; then
+	bibtex $DOCNAME.aux
+fi
+if [ $2 == 2 ]; then
+	biber $DOCNAME.bcf
+fi
+pdflatex $DOCNAME.tex
+pdflatex $DOCNAME.tex
+exit 0
